ceb687bd738f79dc763f2c480fa8f27f522bff9f
Steven <stevengonzalez088@gmail.com>
2023-02-03 12:16:48 -0600

    fix

--- api.py
+++ api.py
@@ -13,12 +13,14 @@ def api():
     data = request.get_json()
     datos = "secret=6LcqoU0kAAAAALRhxRDXBCtwGTc6E-ezB5uAQO0O"+"&response="+data['response']
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data, headers=headers)
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=datos, headers=headers)
     respuesta = response.json()
-
+    print("Respuesta: " + str(respuesta['success']))
+    
     if respuesta['success'] == True:
         return jsonify({'success': True})
     else:
+        print("Error:" + str(respuesta['error-codes']))
         return jsonify({'success': False})
 
 if __name__ == '__main__':
